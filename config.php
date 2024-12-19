b71353d07bd81ac760c01ec002ec581dbb5c506d
beerrkung1 <apiwat.rn@hotmail.com>
2024-12-19 14:45:09 +0700

    Hash

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ return [
     // ตัวอย่างการสร้าง hash:
     // <?php echo password_hash("1234", PASSWORD_DEFAULT);
     // สมมุติได้ค่า: $2y$10$wYV9Ztuh...........
-    'password_hash' => '$2y$10$wYV9Ztuh67aQ7MZOnG3S3udz9GwnHBgHVu2Zr5yzsAQy7K.1YZ90m',
+    'password_hash' => '$2y$12$WLOzzTucgIh6KpnYF9ECAOSf.nRMxU6bTCtC57D0RSWYGEK1K1Gu6',
 
     'upload_directory' => 'D:\\uploads\\', 
     'upload_log' => __DIR__ . DIRECTORY_SEPARATOR . 'upload_log.txt'
