cbaa824fa696dfe07f8161620c8ec0921b898cf4
beerrkung1 <apiwat.rn@hotmail.com>
2024-12-19 15:56:10 +0700

    password

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'username' => 'admin',
-    'password_hash' => '$2y$12$gOTzAsSVaKHo6j4/kovAheSmBtxaA/FgzF2RnaXAGjsDLGPHAynJe',
+    'password_hash' => '$2y$12$WLOzzTucgIh6KpnYF9ECAOSf.nRMxU6bTCtC57D0RSWYGEK1K1Gu6',
     'upload_directory' => 'D:\\Project Data\\',
     'upload_log' => __DIR__ . DIRECTORY_SEPARATOR . 'upload_log.txt'
 ];
