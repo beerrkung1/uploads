7f629ba0d244bc0896fa7b5e4dec145cbe3df8c6
beerrkung1 <apiwat.rn@hotmail.com>
2024-12-23 10:08:44 +0700

    change user

--- config.php
+++ config.php
@@ -3,8 +3,7 @@ return [
     // เปลี่ยนเป็น array ของผู้ใช้หลายคน
     'users' => [
         'admin' => '$2y$12$WLOzzTucgIh6KpnYF9ECAOSf.nRMxU6bTCtC57D0RSWYGEK1K1Gu6', // รหัสผ่านของ admin
-        'tanapornk'  => '$2y$12$lxx4lsFYFA7IuxYbZqk4j.AEa7ze1FJLE6aJoq/Sb2HN9nxuCJXu6',
-      
+        'tanapornk'  => '$2y$12$lxx4lsFYFA7IuxYbZqk4j.AEa7ze1FJLE6aJoq/Sb2HN9nxuCJXu6'
     ],
     'upload_directory' => 'D:\\Project Data\\',
     'upload_log' => __DIR__ . DIRECTORY_SEPARATOR . 'upload_log.txt'
